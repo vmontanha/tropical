c49f299cdd493df2d8a522deb513ba8761bde325
Vinicius Montanha <viniciusmassis01@hotmail.com>
2022-11-28 10:04:02 -0300

    initial develop settings

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -12,7 +12,7 @@ import LinkedIn from "../../img/linkedin.png";
 import Instagram from "../../img/instagram.png";
 import { themeContext } from "../../Context";
 import { motion } from "framer-motion";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FaWhatsapp } from 'react-icons/fa';
 const Intro = () => {
   // Transition
   const transition = { duration: 2, type: "spring" };
@@ -35,7 +35,7 @@ const Intro = () => {
           </span>
         </div>
 
-        <a href="https://api.whatsapp.com/send?phone=5511951442972&text=Ol%C3%A1%2C%20vim%20do%20site%20da%20Tropical.%20Gostaria%20de%20saber%20um%20pouco%20mais%20sobre%20os%20servi%C3%A7os." className="button i-button"><FontAwesomeIcon icon="fa-brands fa-whatsapp" /> Contato</a>
+        <a href="https://api.whatsapp.com/send?phone=5511951442972&text=Ol%C3%A1%2C%20vim%20do%20site%20da%20Tropical.%20Gostaria%20de%20saber%20um%20pouco%20mais%20sobre%20os%20servi%C3%A7os." className="button i-button"><FaWhatsapp /> Contato</a>
 
         {/* social icons */}
         <div className="i-icons">
