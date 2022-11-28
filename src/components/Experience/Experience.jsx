09ea44bbb10186efec77e8e9c37bbb8104176382
Vinicius Montanha <viniciusmassis01@hotmail.com>
2022-11-27 23:30:56 -0300

    footer adjust

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -8,19 +8,18 @@ const Experience = () => {
   return (
     <div className="experience" id='experience'>
       <div className="achievement">
-        {/* darkMode */}
-        <div className="circle" style={{color: darkMode?'var(--orange)':''}}>8+</div>
-        <span  style={{color: darkMode?'white':''}}>years </span>
+        <div className="circle" style={{ color: darkMode ? 'var(--orange)' : '' }}>8+</div>
+        <span style={{ color: darkMode ? 'white' : '' }}>years </span>
         <span>Experience</span>
       </div>
       <div className="achievement">
-        <div className="circle" style={{color: darkMode?'var(--orange)':''}}>20+</div>
-        <span  style={{color: darkMode?'white':''}}>completed </span>
+        <div className="circle" style={{ color: darkMode ? 'var(--orange)' : '' }}>20+</div>
+        <span style={{ color: darkMode ? 'white' : '' }}>completed </span>
         <span>Projects</span>
       </div>
       <div className="achievement">
-        <div className="circle" style={{color: darkMode?'var(--orange)':''}}>5+</div>
-        <span  style={{color: darkMode?'white':''}}>companies </span>
+        <div className="circle" style={{ color: darkMode ? 'var(--orange)' : '' }}>5+</div>
+        <span style={{ color: darkMode ? 'white' : '' }}>companies </span>
         <span>Work</span>
       </div>
     </div>
