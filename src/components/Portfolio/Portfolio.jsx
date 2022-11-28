0f0b4894632c16d92a923346a0ee81748eea2d83
victoriamelo09 <vitoria.melo0901@hotmail.com>
2022-11-28 12:15:17 -0300

    ajustes na Navbar e inclusão de traduções

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -13,7 +13,7 @@ const Portfolio = () => {
   return (
     <div className="portfolio" id="portfolio">
       {/* heading */}
-      <span style={{color: darkMode?'white': ''}}>Recent Projects</span>
+      <span style={{ color: darkMode ? 'white' : '' }}>Recent Projects</span>
       <span>Portfolio</span>
 
       {/* slider */}
