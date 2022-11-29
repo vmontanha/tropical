e8d901757b2e858af996d6b719cef2e20c1cafb0
Natan-devfront <103426213+Natan13vinicius@users.noreply.github.com>
2022-11-29 17:28:15 -0300

    fix natan testimonial text autor depoimento e controle de caracter

--- src/components/Testimonials/Testimonial.jsx
+++ src/components/Testimonials/Testimonial.jsx
@@ -14,23 +14,27 @@ const Testimonial = () => {
   const clients = [
     {
       img: conape,
+      nome: "Valdir Campos Costa  - Conape Auditoria Contábil e Tributária ",
       review:
-        "'Soluções modernas para Design e Tecnologia, é um profissional sério e competente, atua com responsabilidade, Valdir Campos Costa  Conape Auditoria Contábil e Tributária.'",
+        "Soluções modernas para Design e Tecnologia, é um profissional sério e competente, atua com responsabilidade.",
     },
     {
       img: profilePic2,
+      nome: "Valdir Campos Costa  - Conape Auditoria Contábil e Tributária",
       review:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error a nisi placeat quae exercitationem, maiores reiciendis! Eaque dicta minima, iure maiores dolorum sed.",
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium.",
     },
     {
       img: profilePic3,
+      nome: "Valdir Campos Costa  - Conape Auditoria Contábil e Tributária",
       review:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error a nisi placeat quae exercitationem, maiores reiciendis! Eaque dicta minima, iure maiores dolorum sed.",
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error .",
     },
     {
       img: profilePic4,
+      nome: "Valdir Campos Costa  - Conape Auditoria Contábil e Tributária",
       review:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error a nisi placeat quae exercitationem, maiores reiciendis! Eaque dicta minima, iure maiores dolorum sed.",
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error.",
     },
   ];
 
@@ -56,6 +60,7 @@ const Testimonial = () => {
             <SwiperSlide key={index}>
               <div className="testimonial">
                 <img src={client.img} alt="" />
+                <span>{client.nome}</span>
                 <span>{client.review}</span>
               </div>
             </SwiperSlide>
