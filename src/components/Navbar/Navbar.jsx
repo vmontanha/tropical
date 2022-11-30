b24b1d2a9f9873178009d78145d83db3a8c0b552
Vinicius Montanha <viniciusmassis01@hotmail.com>
2022-11-30 18:52:14 -0300

    final commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -45,7 +45,7 @@ const navbar = () => {
           </ul>
         </div>
 
-        <a href="https://api.whatsapp.com/send?phone=5511951442972&text=Ol%C3%A1%2C%20vim%20do%20site%20da%20Tropical.%20Gostaria%20de%20saber%20um%20pouco%20mais%20sobre%20os%20servi%C3%A7os." className="button n-button">Conversar</a>
+        <a href="https://api.whatsapp.com/send?phone=5511939588122&text=Seja%20bem%20vindo!%2C%20como%20podemos%20lhe%20ajudar%20%3F" className="button n-button">Conversar</a>
 
       </div>
     </div>
